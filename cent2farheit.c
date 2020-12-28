ecaacbcc61f020a497cf4128de4a765bd6613fb5
Manuel Duval <manuel.x.duval@gmail.com>
2020-12-28 10:50:16 -0500

    Update cent2farheit.c

--- cent2farheit.c
+++ cent2farheit.c
@@ -5,7 +5,7 @@
 *										 *
 * Date: 12/27/2020					                        *
 *										 *
-* Pupose: convert centigrad to farenheit					 * 
+* Pupose: convert Centigrad to Fahrenheit unit					 * 
 *										 *
 * Usage: invoke the program and enter a numeric value			 *
 *  										 *
@@ -14,7 +14,7 @@
 #include <string.h>
 
 char centigrade[100]; /* input temperature in Centrigrade unit */
-int farenheit; /* temperature expressed in Farenheit unit */
+int fahrenheit; /* temperature expressed in Fahrenheit unit */
 
 int main()
 {
@@ -23,15 +23,15 @@ int main()
   fgets(centigrade, sizeof(centigrade), stdin);
   
   /* getting the integer value from the input string using the sscanf function */
-  sscanf(centigrade, "%d", &farenheit);
+  sscanf(centigrade, "%d", &fahrenheit);
   
   /* assigning the farenheit value */
-  farenheit = 9/5 * farenheit + 32;
+  fahrenheit = 9/5 * fahrenheit + 32;
   
   /* removing the trailing end of line character from the centigrade variable */
   centigrade[strlen(centigrade) -1] = '\0';
   
-  printf("Temperature in Centigrade unit:\t%s\tTemperature in Farenheit unit: %d\t\n", centigrade, farenheit);
+  printf("Temperature in Centigrade unit:\t%s\tTemperature in Fahrenheit unit: %d\t\n", centigrade, fahrenheit);
 
   return (0);
 }
